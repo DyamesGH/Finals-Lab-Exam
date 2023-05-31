93270f23d106f9dc2369d647606f5a8ac4360691
James Meneses <james.meneses112603@gmail.com>
2023-05-31 16:18:03 +0800

    Initial commit

---
+++ src/Item.java
@@ -0,0 +1,39 @@
+
+public class Item {
+
+	private int id;
+	private String description;
+	private int price;
+	private int stock;
+	
+	public Item (int id, String desc, int price, int stock){
+		this.id= id;
+		this.description = desc;
+		this.price = price;
+		this.stock = stock;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}	
+	
+	
+	
+	
+}
---
+++ src/ItemNode.java
@@ -0,0 +1,38 @@
+
+public class ItemNode {
+	private ItemNode next;
+	private ItemNode previous;
+	private Item data;
+	
+	
+	
+	public ItemNode(Item data) {
+		this.data = data;
+		next = null;
+		previous = null;
+	}
+	
+	public ItemNode getNext() {
+		return next;
+	}
+	public ItemNode getPrevious() {
+		return previous;
+	}
+	public Item getData() {
+		return data;
+	}
+	public void setNext(ItemNode next) {
+		this.next = next;
+	}
+	public void setPrevious(ItemNode previous) {
+		this.previous = previous;
+	}
+	public void setData(Item data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+
+}
