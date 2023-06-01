02cfd18f8432d6ef326a4d6fb34a4b87bd7e4303
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2023-06-01 17:49:42 +0800

    Update ItemList.java

--- src/ItemList.java
+++ src/ItemList.java
@@ -1,20 +1,20 @@
 
 public class ItemList {
-	
+
 	private ItemNode head;
 	private ItemNode tail;
 	private static int itemIdCounter;
-	
+
 	public ItemList() {
 		head = null;
 		tail = null;
-		itemIdCounter=1;
+		itemIdCounter = 1;
 	}
-	
+
 	public static int getItemIdCounter() {
 		return itemIdCounter;
 	}
-	
+
 	public void add(ItemNode node) {
 		if (head == null) {
 
@@ -30,26 +30,27 @@ public class ItemList {
 			itemIdCounter++;
 		}
 	}
-	
+
 	public String displayItems() {
-		System.out.printf("%-7s %-11s %-5s %-5s %n", "Item ID","Description","Price","Stock");
+		System.out.printf("%-25s %-25s %-25s %-25s %n", "Item ID", "Description", "Price", "Stock");
 		ItemNode currentNode = head;
-		while(currentNode != null) {
-			System.out.printf("%-7d %-11s %-5d  %-5d %n", currentNode.getData().getId(),currentNode.getData().getDescription(),currentNode.getData().getPrice(),currentNode.getData().getStock() );
+		while (currentNode != null) {
+			System.out.printf("%-25d %-25s %-25d  %-25d %n", currentNode.getData().getId(),
+					currentNode.getData().getDescription(), currentNode.getData().getPrice(),
+					currentNode.getData().getStock());
 			currentNode = currentNode.getNext();
 		}
 		return "";
 	}
-	
-	public  Item getItem(int id) {
+
+	public Item getItem(int id) {
 		ItemNode currentNode = head;
-		
-		while(currentNode != null) {
-			if(currentNode.getData().getId() == id) {
+
+		while (currentNode != null) {
+			if (currentNode.getData().getId() == id) {
 				return currentNode.getData();
-			}else {
-				currentNode = currentNode.getNext();
 			}
+			currentNode = currentNode.getNext();
 		}
 		return null;
 	}
