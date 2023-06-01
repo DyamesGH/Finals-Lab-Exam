a15ddc00f8d44e36ba34b6e779c467c5393e6ec5
John Emmanuel Sales <66854142+JE-Sales@users.noreply.github.com>
2023-06-01 17:51:37 +0800

    Update TransactionList.java

--- src/TransactionList.java
+++ src/TransactionList.java
@@ -1,3 +1,41 @@
 
 public class TransactionList {
+	private int transactionID = 1;
+	private TransactionNode head;
+	private TransactionNode tail;
+
+	public TransactionNode getHead() {
+		return head;
+	}
+
+	public void setHead(TransactionNode head) {
+		this.head = head;
+	}
+
+	public TransactionNode getTail() {
+		return tail;
+	}
+
+	public void setTail(TransactionNode tail) {
+		this.tail = tail;
+	}
+
+	public int getTransactionID() {
+		return transactionID;
+	}
+
+	public void addTransaction(TransactionNode newTransaction) {
+		newTransaction.setTransactionID(transactionID);
+
+		if (head == null) {
+			head = newTransaction;
+			tail = head;
+		} else {
+			tail.setNext(newTransaction);
+			tail = tail.getNext();
+		}
+
+		transactionID++;
+	}
+
 }
